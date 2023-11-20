17e412e3c4859066ec9ee64d1b32b6b1e8ec9911
alalalala309 <150366298+alalalala309@users.noreply.github.com>
2023-11-21 02:31:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="title" content="Hacked By MalIndo Nusantara">
+    <meta name="description" content="United Among Muslim Hackers">
+    <meta name="keywords" content="Mal Indo Nusantara">
+    <meta name="image" content="https://i.ibb.co/0KfMF5R/Main-Logo.png">
+    <meta name="googlebot" content="index,follow">
+    <meta name="robots" content="index, follow">
+    <meta name="robots schedule" content="auto">
+    <meta name="distribution" content="global">
+    <link rel="icon" type="image/x-icon" href="https://i.ibb.co/0KfMF5R/Main-Logo.png">
+    <link href='https://fonts.googleapis.com/css?family=Sedgwick+Ave' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <title>Hacked By MalIndo Nusantara</title>
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Paytone+One");
+      body {
+        background-color: #000000;
+        background-image:url('');
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+      #wrapper {
+        text-align: center;
+		    left: 0;
+		    right: 0;
+		    top: 43%
+      }
+      a { 
+        color: red; 
+      }
+      g {
+        color: green;
+      }
+      text {
+        font-family: Righteous;
+        color: aliceblue;
+      }
+    </style>
+    <script>
+      window.addEventListener("mouseover", play);
+      window.addEventListener("click", play);
+
+      function play() {
+      document.getElementById("a").play();
+    }
+    </script>
+    <div id="wrapper">
+      <img src="https://i.ibb.co/c2r0rgw/Main-Logoo.png" height="225px">
+      <!--<img src="https://cdn.pixabay.com/animation/2022/09/13/17/55/17-55-41-668_512.gif" height="225px">-->
+      <!-- <img src="http://www.animated-gifs.fr/category_sports/sports-olympics-flags-boys/0112.gif" height="225px"> -->
+      <b>
+        <br>
+        <p>
+      <font face="Paytone One" style="color: white;">
+        Greetings The Government and World. <a>We Are Hacktivist Of Garuda And DragonForce Malaysia | MalIndo Nusantara</a></b>
+    </font>
+        <text>
+        <br>
+        <br>
+      Because we are the voice of <g>Palestine</g>, The voice of Muslim and we will not remain silent !
+        <br>
+        A cursory glance at various Western media outlets this month on the latest round of Israeli
+      aggression against <g>Palestinians</g> has more often than not, revealed ongoing bias or false equivalencies in the
+      framing of their coverage. <br> 
+      They frequently reported this issue as a conflict and criticized <g>HAMAS</g> as the cause of such an issue. <br>
+      However, this is not a conflict. <br>
+      Instead, it was the oppression of Palestine, a <a>GENOCIDE</a>. 
+      <br>
+      <br>
+      Your vital databases will be
+      exposed along with secret documents to show the true color of <a>Zionists</a>. <br>
+      Let the world know the violent acts of
+      Zionism ! 
+      <br>
+      <br>
+       #<g>OpsIsrael</g> Engaged !!   
+      <br>
+      <br>
+        Greetz : <br> <g>Malaysian Hacker</g> Feat <g>Indonesian Hacker</g>
+      <br>
+      <br>
+      Copyright &copy; 2023 MalIndo Nusantara. All Rights Reserved.
+        </p>
+        <audio id="a" src="https://kosred.com/a/grcros.mp3" autoplay></audio>
+      </body>
+      </b>
+    </div>
+    </head>
+  </body>
+</html>
